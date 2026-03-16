688bd1df9d1f9ff12e0c6902a0a6bf85d01be1c8
Tony Moreira <tonymoreira@wmsexpert.com.br>
2026-03-16 13:04:06 -0300

    add api route

--- api/runrun.js
+++ api/runrun.js
@@ -1,29 +1,18 @@
 export default async function handler(req, res) {
-  try {
 
-    const response = await fetch(
-      "https://runrun.it/api/v1.0/tasks?board_id=597967&limit=100",
-      {
-        method: "GET",
-        headers: {
-          "App-Token": process.env.RUNRUN_APP_TOKEN,
-          "User-Token": process.env.RUNRUN_USER_TOKEN,
-          "User-Agent": "Mozilla/5.0",
-          "Accept": "application/json"
-        }
+  const response = await fetch(
+    "https://runrun.it/api/v1.1/tasks?board_id=597967&limit=100",
+    {
+      method: "GET",
+      headers: {
+        "App-Token": process.env.RUNRUN_APP_TOKEN,
+        "User-Token": process.env.RUNRUN_USER_TOKEN,
+        "Accept": "application/json"
       }
-    );
+    }
+  );
 
-    const data = await response.text();
+  const text = await response.text();
 
-    res.status(response.status).send(data);
-
-  } catch (error) {
-
-    res.status(500).json({
-      error: "Erro conexão Runrun",
-      message: error.message
-    });
-
-  }
+  res.status(response.status).send(text);
 }
