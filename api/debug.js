82d070e16f8a7ca2209c20eeede41ff539cbd310
Tony Moreira <tonymoreira@wmsexpert.com.br>
2026-03-16 12:53:32 -0300

    fix runrun headers

--- api/debug.js
+++ api/debug.js
@@ -1,6 +1,6 @@
-export default function handler(req, res) {
-  res.status(200).json({
+export default function handler(req,res){
+  res.json({
     app: process.env.RUNRUN_APP_TOKEN,
     user: process.env.RUNRUN_USER_TOKEN
-  });
+  })
 }
--- api/runrun.js
+++ api/runrun.js
@@ -1,15 +1,18 @@
 export default async function handler(req, res) {
   try {
 
-    const url = "https://runrun.it/api/v1.0/tasks?board_id=597967&limit=100";
-
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "App-Token": process.env.RUNRUN_APP_TOKEN,
-        "User-Token": process.env.RUNRUN_USER_TOKEN
+    const response = await fetch(
+      "https://runrun.it/api/v1.0/tasks?board_id=597967&limit=100",
+      {
+        method: "GET",
+        headers: {
+          "App-Token": process.env.RUNRUN_APP_TOKEN,
+          "User-Token": process.env.RUNRUN_USER_TOKEN,
+          "User-Agent": "Mozilla/5.0",
+          "Accept": "application/json"
+        }
       }
-    });
+    );
 
     const data = await response.text();
 
@@ -18,7 +21,7 @@ export default async function handler(req, res) {
   } catch (error) {
 
     res.status(500).json({
-      error: "Erro ao conectar com Runrun",
+      error: "Erro conexão Runrun",
       message: error.message
     });
 
