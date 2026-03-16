991c1b1e5954b966c2e1fa15ef0fa9fdfb96b8ed
Tony Moreira <tonymoreira@wmsexpert.com.br>
2026-03-16 14:46:26 -0300

    fix vercel config

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import './App.css';
 import Header from './Components/Header';
-import Main from './Components/Main';
+import Main from './Components/Main'; // Agora importa o index.js da pasta Main
 import style from './style.module.css';
-
 
 function App() {
   return (
